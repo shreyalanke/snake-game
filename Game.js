b794b1928dda579f1d938cd9c965eab735bd9ba3
shreyalanke <lankeshreya@gmail.com>
2025-12-21 14:13:03 +0530

    Added core logic

--- Game.js
+++ Game.js
@@ -2,10 +2,12 @@ class Game {
     constructor(gameSize) {
         this.score = 0;
         this.board = [];
+        this.isGameOver = false;
         this.height = gameSize;
         this.width = gameSize;
         this.direction = 'N'
         this.highest = 3;
+
         for (let i = 0; i < this.height; i++) {
             let row = [];
             for (let j = 0; j < this.width; j++) {
@@ -14,47 +16,159 @@ class Game {
             this.board.push(row);
         }
 
-        this.board[Math.floor(this.height / 2)][Math.floor(this.width / 2)] = 3;
+        this.board[Math.floor(this.height / 2)][Math.floor(this.width / 2)] = 10;
 
     }
+
+    getIsGameOver() {
+        return this.isGameOver;
+    }
+
 
     update() {
-        let head = {x:-1,y:-1,value:0};
-        for(let i = 0; i < this.board.length; i++ ){
-            for(let j = 0; j < this.board[i].length; j++){
-                    if(this.board[i][j]>head.value){
-                        head.value=this.board[i][j];
-                        head.x=i;
-                        head.y=j;
+        let head = { x: -1, y: -1, value: 0 };
+        let foodFlag = false;
+        //(Shreya): Finding head position & food
+        for (let i = 0; i < this.board.length; i++) {
+            for (let j = 0; j < this.board[i].length; j++) {
+                if (this.board[i][j] > head.value) {
+                    head.value = this.board[i][j];
+                    head.x = i;
+                    head.y = j;
+                }
+                if (this.board[i][j] < 0) {
+                    foodFlag = true;
+                }
+            }
+
+        }
+        if (!foodFlag) {
+            let randomX = Math.floor(random() * this.width);
+            let randomY = Math.floor(random() * this.height);
+
+            while (this.board[randomX][randomY] > 0) {
+                randomX = Math.floor(random() * this.width);
+                randomY = Math.floor(random() * this.height);
+            }
+
+            this.board[randomX][randomY] = -1;
+        }
+
+
+        let isEating = false;
+
+        if (this.direction == 'N') {
+            if (this.board[head.x][head.y - 1] == -1) {
+                isEating = true;
+            }
+        }
+
+        if (this.direction == 'S') {
+            if (this.board[head.x][head.y + 1] == -1) {
+                isEating = true;
+            }
+        }
+
+        if (this.direction == 'E') {
+            if (this.board[head.x + 1][head.y] == -1) {
+                isEating = true;
+            }
+        }
+
+        if (this.direction == 'W') {
+            if (this.board[head.x - 1][head.y] == -1) {
+                isEating = true;
+            }
+        }
+
+
+
+
+        //(Shreya): Updating snake body
+        if (isEating) {
+            head.value++;
+
+        } else {
+            for (let i = 0; i < this.board.length; i++) {
+                for (let j = 0; j < this.board[i].length; j++) {
+                    if (this.board[i][j] > 0) {
+                        this.board[i][j]--;
+
                     }
-            }
-        }
-         for(let i = 0; i < this.board.length; i++ ){
-            for(let j = 0; j < this.board[i].length; j++){
-                  if(this.board[i][j]>0){
-                    this.board[i][j]--;
-
-                  }
+                }
             }
         }
 
-        if(this.direction=='N'){
-            this.board[head.x][head.y-1]=head.value;
+        //(Shreya): Moving head based on direction
+        if (this.direction == 'N') {
+            if (head.y - 1 < 0) {
+                this.isGameOver = true;
+                return;
+            }
+            if (this.board[head.x][head.y - 1] > 0) {
+                this.isGameOver = true;
+                return;
+            }
+            this.board[head.x][head.y - 1] = head.value;
         }
-        if(this.direction=='S'){
-            this.board[head.x][head.y+1]=head.value;
+        if (this.direction == 'S') {
+            if (head.y + 1 > this.height - 1) {
+                this.isGameOver = true;
+                return;
+            }
+            if (this.board[head.x][head.y + 1] > 0) {
+                this.isGameOver = true;
+                return;
+            }
+            this.board[head.x][head.y + 1] = head.value;
         }
-        if(this.direction=='E'){
-            this.board[head.x+1][head.y]=head.value;
+        if (this.direction == 'E') {
+            if (head.x + 1 > this.width - 1) {
+                this.isGameOver = true;
+                return;
+            }
+            if (this.board[head.x + 1][head.y] > 0) {
+                this.isGameOver = true;
+                return;
+            }
+            this.board[head.x + 1][head.y] = head.value;
         }
-        if(this.direction=='W'){
-            this.board[head.x-1][head.y]=head.value;
+        if (this.direction == 'W') {
+            if (head.x - 1 < 0) {
+                this.isGameOver = true;
+                return;
+            }
+            if (this.board[head.x - 1][head.y] > 0) {
+                this.isGameOver = true;
+                return;
+            }
+            this.board[head.x - 1][head.y] = head.value;
         }
-        this.highest=head.value;
+        this.highest = head.value;
     }
 
-    setDirection(direction){
-        this.direction=direction;
+    setDirection(direction) {
+        if (direction == 'N') {
+            if (this.direction != 'S') {
+                this.direction = direction;
+            }
+        }
+        if (direction == 'S') {
+            if (this.direction != 'N') {
+                this.direction = direction;
+            }
+        }
+        if (direction == 'E') {
+            if (this.direction != 'W') {
+                this.direction = direction;
+            }
+        }
+        if (direction == 'W') {
+            if (this.direction != 'E') {
+                this.direction = direction;
+            }
+        }
+
     }
 
 
@@ -65,10 +179,13 @@ class Game {
                 if (this.board[i][j] == 0) {
                     fill(0);
                 }
+                if (this.board[i][j] == -1) {
+                    fill(255, 0, 0);
+                }
                 if (this.board[i][j] > 0) {
-                    fill((this.board[i][j]/this.highest)*255,
-                    (this.board[i][j]/this.highest)*255,
-                    255);
+                    fill((this.board[i][j] / this.highest) * 255,
+                        (this.board[i][j] / this.highest) * 255,
+                        255);
                 }
                 rect(i * cellSize, j * cellSize, cellSize, cellSize);
 
--- script.js
+++ script.js
@@ -1,17 +1,23 @@
 
 let game;
-let gameSize = 25;
+let gameSize = 50;
 let cellSize = 500 / gameSize;
 
 function setup() {
    createCanvas(500, 500);
-   frameRate(1);
+   frameRate(5);
    background(220);
    game = new Game(gameSize);
 }
 
 function draw() {
+
    game.update();
+   if (game.getIsGameOver()) {
+      background(255);
+      text("Maa Chud Gayi!!!", 200, 250);
+      return;
+   }
    game.drawBoard(cellSize);
 }
 
